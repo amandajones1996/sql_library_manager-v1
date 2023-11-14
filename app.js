6875a0383615f28124c393975830d7744555a150
amandajones1996 <jonesamanda996@gmail.com>
2023-11-14 15:50:27 -0800

    working on debuging errors

---
+++ app.js
@@ -0,0 +1,88 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// import the sequelize instance 
+var { sequelize } = require('./models');
+
+var indexRouter = require('./routes/index');
+var booksRouter = require('./routes/books');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/books', booksRouter);
+
+// testing connection to database
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connection established")
+    // syncing models
+    return sequelize.sync()
+  })
+  .then(() => {
+    console.log('Models are synced correctly')
+  })
+  .catch(err => {
+    console.log('Unable to sync to the database', err)
+  })
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  // Create a new error object with a 404 status and a friendly message
+  const err = new Error('The requested page could not be found');
+  err.status = 404;
+  
+  // Render 'page-not-found' view, passing the error object
+  res.status(404).render('page-not-found', { error: err });
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // If there's no status code on the error, set it to 500
+  if (!err.status) {
+    err.status = 500;
+  }
+  // If there's no custom message, set a default message
+  if (!err.message || err.status === 500) {
+    err.message = 'An unexpected error occurred on the server';
+  }
+
+  console.error(`Error (${err.status}): ${err.message}`);
+
+  // set locals, only providing error in development
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status).render('error', { error: err });
+});
+
+module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+const Book  = require('../models').Book;
+var { sequelize } = require('../models');
+
+/* Redirect home to /books route */
+// router.get('/', function(req, res, next) {
+//   res.redirect('/books');
+// });
+
+/* GET books listing. */
+router.get('/', async function(req, res, next) {
+  try {
+    const books = await Book.findAll();
+    // res.json(books);
+    res.render('index', { books: books }); 
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* GET the form for creating a new book. */
+router.get('/new', function(req, res, next) {
+  res.render('new-book'); 
+});
+
+/* POST a new book to the database. */
+router.post('/new', async function(req, res, next) {
+  try {
+    await Book.create(req.body);
+    res.redirect('/books');
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      res.render('new-book', {
+        book: Book.build(req.body),
+        errors: error.errors
+      });
+    } else {
+      next(error);
+    }
+  }
+});
+
+/* GET individual book details. */
+router.get('/:id', async function(req, res, next) {
+  try {
+    console.log(req.params.id)
+    const book = await Book.findByPk(req.params.id);
+    console.log(book)
+    if (book) {
+      res.render('update-book', { book, title: book.title }); 
+    } else {
+      next(createError(404));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* POST update to a book. */
+router.post('/:id', async function(req, res, next) {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect('/books');
+    } else {
+      next(createError(404));
+    }
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      const book = await Book.build(req.body);
+      book.id = req.params.id; 
+      res.render('update-book', {
+        book: book,
+        errors: error.errors
+      });
+    } else {
+      next(error);
+    }
+  }
+});
+
+/* POST delete a book. */
+router.post('/:id/delete', async function(req, res, next) {
+  try {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect('/books');
+    } else {
+      next(createError(404));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
