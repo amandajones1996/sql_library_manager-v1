68a2e234cf1bdf9259925066c39dd97f88e6267f
amandajones1996 <jonesamanda996@gmail.com>
2023-12-12 10:23:44 -0500

    delete commented out code

--- app.js
+++ app.js
@@ -38,22 +38,6 @@ sequelize.authenticate()
     console.log('Unable to sync to the database', err)
   })
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   // Create a new error object with a 404 status and a friendly message
